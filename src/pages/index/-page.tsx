6ab96549c355af4964daf602151a13a6cef57eb5
icheng <yicheng-me@qq.com>
2023-06-08 07:54:29 +0000

    chore: 调整代码

--- src/pages/index/-page.tsx
+++ src/pages/index/-page.tsx
@@ -1,20 +1,18 @@
-import { Helmet } from "react-helmet-async";
-import styled from "styled-components";
-
-
+import { Helmet } from 'react-helmet-async';
+import styled from 'styled-components';
 
 const Div = styled.div`
 
 
-`
+`;
 
-export default function() {
-
-
-    return <Div>
-        <Helmet>
-            <title>首页</title>
-        </Helmet>
-        首页
+export default function _ () {
+  return (
+    <Div>
+      <Helmet>
+        <title>首页</title>
+      </Helmet>
+      首页
     </Div>
+  );
 }
